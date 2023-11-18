4799349e27366e9f81c205d37fed52a02830ef4c
Rahul Jha <rahulkumarjha08101999@gmail.com>
2023-11-18 15:33:43 +0530

    Code changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Rahul Jha");
+
+        String ans = obj.getName();
+        System.out.println(ans);
+
+
+    }
   
 }
